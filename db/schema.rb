cfabe38a162c108d281d18b7b1fbc74bc2f9c783
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-11 09:14:01 +0100

    Created the schema ERD diagram

--- db/schema.rb
+++ db/schema.rb
@@ -10,3 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord ::Schema.define do
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |i|
+    i.bigint "author_id", null: false
+    i.bigint "post_id", null: false
+    i.text "text"
+    i.datetime "created_at", null: false
+    i.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |i|
+    i.bigint "author_id", null: false
+    i.bigint "post_id", null: false
+    i.datetime "created_at", null: false
+    i.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |i|
+    i.string "name", null: false
+    i.photo "photo"
+    i.string "bio"
+    i.integer "posts_counter"
+    i.datetime "created_at", null: false
+    i.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |i|
+    i.bigint "author_id", null: false
+    i.string "title"
+    i.text "text"
+    i.datetime "created_at", null: false
+    i.datetime "updated_at", null: false
+    i.integer "comments_counter"
+    i.integer "likes_counter"
+  end
+
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
+end
