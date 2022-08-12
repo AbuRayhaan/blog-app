fc15f026d9cebb19654fbc8f6b35c087742275b1
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-12 20:42:14 +0100

    Added method to class User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+
+  # method that returns the 3 most recent posts for a given user
+
+  def recent_post
+    posts.limit(3).order(created_at: :desc)
+  end
 end
