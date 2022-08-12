33a9ec9255db72e7a8d6366a09fbfe0ae427a9d7
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-12 20:45:23 +0100

    "Migration Completed for Comments"

--- db/migrate/20220812181341_create_comments.rb
+++ db/migrate/20220812181341_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :author_id, foreign_key: true
-      t.integer :post_id, foreign_key: true
+      t.integer :author_id
+      t.integer :post_id
       t.text :text
 
       t.timestamps
