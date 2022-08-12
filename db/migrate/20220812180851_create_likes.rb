e581551403e807bf8365310f898729c694936ba1
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-12 20:45:05 +0100

    "Migration Completed for Likes"

--- db/migrate/20220812180851_create_likes.rb
+++ db/migrate/20220812180851_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.integer :author_id, foreign_key: true
-      t.integer :post_id, foreign_key: true
+      t.integer :author_id
+      t.integer :post_id
 
       t.timestamps
     end
