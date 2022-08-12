4370e663d2a6719cd471f505e53bd3c1f5084040
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-12 22:01:42 +0100

    Added validations to User models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  validates :name, presence: true
+  validates :post_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
   # method that returns the 3 most recent posts for a given user
 
   def recent_post
