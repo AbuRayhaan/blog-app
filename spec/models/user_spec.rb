635bfd89e30cc6f29384549afb265d856b7f73fc
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-12 22:05:33 +0100

    created test for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+# require_relative '../config/environment'
+
+RSpec.describe User, type: :model do
+  subject do
+    User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  end
+
+  before :each do
+    subject.save
+  end
+
+  it 'Name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Post counter must be greater or equal to zero' do
+    subject.post_counter = -1
+    expect(subject).to_not be_valid
+  end
+end
