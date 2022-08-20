030d085efb021c5cd3ca6dbb4f00e9dc2ab48a89
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-20 10:58:26 +0100

    Modified LikesController

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,13 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @like = @post.likes.build
-    @likes.user_id = current_user.id
+    @like.author = current_user
     respond_to do |format|
       if @like.save
+        # @like.update_likes_counter
         # flash[:success] = 'Like Created'
       end
-      format.html { redirect_to_user_post_path(current_user, @post) }
+      format.html { redirect_to user_post_path(current_user, @post) }
     end
   end
 end
