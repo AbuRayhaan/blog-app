54fcdb81829701148af61fb71fded9b9271afeb3
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-20 06:49:17 +0100

    Added new and create actions/methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to_user_posts_path(current_user)
+      #flash[:success] = 'Post Created'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
