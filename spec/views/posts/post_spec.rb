179692bc31a369e3dcae2726b596da47bf026b51
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-25 18:42:50 +0100

    Modified tests for Post index and show pages

--- spec/views/posts/post_spec.rb
+++ spec/views/posts/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :view do
   before :each do
-    @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    @user = User.create(name: 'Tom', photo: 'avatal.jpg', bio: 'Teacher from Mexico.')
     @user.posts.create(title: 'Title', text: 'Body')
     visit user_post_path(@user, @user.posts.first)
   end
@@ -11,44 +11,46 @@ RSpec.describe Post, type: :view do
     it 'Should display post\s title' do
       expect(page).to have_content('Title')
     end
-    it 'Should display post author name' do
-      expect(page).to have_content('by Tom')
+    it 'should display posts comment counter' do
+      expect(page).to have_content('Comments: 0')
     end
-    it 'should display post s comment counter' do
-      expect(page).to have_content('0 comments')
-    end
-    it 'should display post s likes counter' do
-      expect(page).to have_content('0 likes')
+    it 'should display posts likes counter' do
+      expect(page).to have_content('Likes: 0')
     end
     it 'Should display post\s text' do
       expect(page).to have_content('Body')
     end
-    it 'Should display comment user name' do
-      @user.posts.first.comments.create(text: 'Comment', user: @user)
-      visit user_post_path(@user, @user.posts.first)
-      expect(page).to have_content('Comment by: Tom')
+  end
+
+  context 'Post index page' do
+    it 'Should display username' do
+      visit user_posts_path(@user.id)
+      expect(page).to have_content('Tom')
     end
-  end
 
-  it 'Should content Add post button' do
-    visit user_path(@user.id)
-    expect(page).to have_content('Title')
-    expect(page).to have_content('Add post')
-  end
+    it 'should display user photo' do
+      visit user_posts_path(@user.id)
+      expect(page.html).to include('avatal')
+    end
 
-  it 'Should display add post form' do
-    visit user_path(@user.id)
-    click_link 'Add post'
-    expect(current_path).to eq(new_post_path)
-    expect(response).to render_template('new')
-  end
+    it 'Should display number of posts each user has' do
+      visit user_posts_path(@user.id)
+      expect(page).to have_content('Number of posts: 1')
+    end
 
-  it 'Should create new post' do
-    visit new_post_path
-    fill_in 'post[title]', with: 'New Title'
-    fill_in 'post[text]', with: 'New Body'
-    click_button 'Submit new post'
-    expect(current_path).to eq(user_posts_path(@user.id))
-    expect(page).to have_content('New Title')
+    it 'Should display title of posts each user has' do
+      visit user_posts_path(@user.id)
+      expect(page).to have_content('Title')
+    end
+
+    it 'Should display body of posts each user has' do
+      visit user_posts_path(@user.id)
+      expect(page).to have_content('Body')
+    end
+
+    it 'Should allow user to add a post' do
+      visit user_posts_path(@user.id)
+      expect(page).to have_content('New Post')
+    end
   end
 end
