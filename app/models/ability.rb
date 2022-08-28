59dc177212519e5be6344582fd1626d1dd43bf1f
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-28 18:14:27 +0100

    Modified Ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
@@ -7,7 +5,7 @@ class Ability
     user ||= User.new
 
     can :read, :all
-    
+
     if user.is? :admin
       can :manage, :all
       can :access, :rails_admin
